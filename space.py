92b30aa640255bb04420fcc2733be16b0d6c4ea0
jurbster <jurbster15@yahoo.com>
2024-09-20 10:31:53 -0500

    add player icon

--- space.py
+++ space.py
@@ -1,10 +1,36 @@
 #Space Invaders Game using PyGame
 
 import pygame
+
+from pygame.locals import(
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+    RLEACCEL,
+)
+
 pygame.init()
 
+screenWidth = 800
+screenHeight = 600
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.image.load("spaceship.png").convert()
+        self.surf = pygame.transform.scale(self.surf, (60, 60))
+        self.surf.set_colorkey((255, 255, 255,), RLEACCEL)
+        self.rect = self.surf.get_rect()
+
 #Set up the game window.
-screen = pygame.display.set_mode([800, 600])
+screen = pygame.display.set_mode([screenWidth, screenHeight])
+
+player = Player()
+
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
 
 running = True
 while running:
@@ -18,8 +44,8 @@ while running:
 
     pygame.draw.circle(screen, (0, 0, 255), (400, 300), 25)
 
+    for entity in all_sprites:
+        screen.blit(entity.surf, entity.rect)
+
     #Flip the display - nothing shows up without this.
     pygame.display.flip()
-
-#Quit the game if the close button is pressed.
-pygame.quit()
